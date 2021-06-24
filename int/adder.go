1855a0a93b0087dce40ffacf47d2d451cf01ebf1
JunLog <junlog@foxmail.com>
2021-06-24 10:48:14 +0800

    完成《整数》章节

---
+++ int/adder.go
@@ -0,0 +1,6 @@
+package integers
+
+// 这里的注释好出现在 Go Doc,就像看标准库的文档一样
+func Add(x, y int) int {
+	return x + y
+}
---
+++ int/adder_test.go
@@ -0,0 +1,34 @@
+package integers
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 测试函数命名以 Test 开头
+func TestAdder(t *testing.T) {
+	sum := Add(2, 2)
+	expected := 4
+	if sum != expected {
+		t.Errorf("expected '%d' but got '%d'", expected, sum)
+	}
+}
+
+// 示例，反映代码的实际功能 函数名 Example 开头，
+// 会有一行 Output 的注释行,删除这行的话这个函数不会执行
+// 这个函数也会出现在 godoc 中
+func ExampleAdd() {
+	sum := Add(1, 5)
+	fmt.Println(sum)
+	//Output: 6
+
+}
+
+// 命令 ： go doc
+// 查看包的所有函数
+
+// 命令(未调通) ： godoc -http:=6060 -play
+// 从 1.13 版本后被移除，安装 go get golang.org/x/tools/cmd/godoc
+
+// 命令 go test -v
+// -v 输出测试详细信息
