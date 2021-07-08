a39f17de8071f92944aa7ece6764e12f72549e31
JunLog <junlog@foxmail.com>
2021-07-08 15:23:16 +0800

    完成《结构体，方法和和接口》章节

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -55,19 +55,42 @@ func TestArea(t *testing.T) {
 
 	// =============================================
 	// 表格驱动
+	// areaTests := []struct {
+	// 	shape Shape
+	// 	want  float64
+	// }{
+	// 	{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+	// 	{shape: Circle{Radius: 10}, want: 314.1592653589793},
+	// 	{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	// }
+	// for _, tt := range areaTests {
+	// 	got := tt.shape.Area()
+	// 	if got != tt.want {
+	// 		// tt.shape 结构体中域的值，可以一眼看出被测试的属性
+	// 		t.Errorf("%#v 获得 %.2f 预期 %.2f", tt.shape, got, tt.want)
+	// 	}
+	// }
+
+	// =============================================
+	// 运行列表中指定的测试用例 go test -run TestArea/Rectangle
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
-		{shape: Circle{Radius: 10}, want: 314.1592653589793},
-		{shape: Triangle{Base: 2, Height: 6}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 16}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 16}, hasArea: 36.0},
 	}
+
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			// tt.shape 结构体中域的值，可以一眼看出被测试的属性
-			t.Errorf("%#v 获得 %.2f 预期 %.2f", tt.shape, got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				//  %v 结构体；%+v 结构体(包含字段名)；%#v(产出该值的代码段)
+				// %g 根据情况选择 %e 或 %f 以产生更紧凑的（无末尾的0）
+				t.Errorf("%#v 获取到 %g 预期 %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
